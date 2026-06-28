a90d0fdf384f116d5cb95bc9f029cdc6761ab6f1
JotaFerreira01 <jonathan.ramalhof15@gmail.com>
2026-06-27 21:32:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect
+import json
+import os
+
+app = Flask(__name__)
+ARQUIVO = "produtos.json"
+
+def ler_produtos():
+    if os.path.exists(ARQUIVO):
+        with open(ARQUIVO, "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+    return []
+
+def salvar_produtos(produtos):
+    with open(ARQUIVO, "w", encoding="utf-8") as arquivo:
+        json.dump(produtos, arquivo, ensure_ascii=False, indent=4)
+
+@app.route("/")
+def inicio():
+    produtos = ler_produtos()
+    return render_template("index.html", produtos=produtos)
+
+@app.route("/adicionar", methods=["POST"])
+def adicionar():
+    produtos = ler_produtos()
+
+    novo_produto = {
+        "id": len(produtos) + 1,
+        "nome": request.form["nome"],
+        "categoria": request.form["categoria"],
+        "quantidade": float(request.form["quantidade"]),
+        "preco": float(request.form["preco"]),
+        "tipo_preco": request.form["tipo_preco"]
+    }
+
+    produtos.append(novo_produto)
+    salvar_produtos(produtos)
+    return redirect("/")
+
+@app.route("/excluir/<int:id>")
+def excluir(id):
+    produtos = ler_produtos()
+    produtos = [p for p in produtos if p["id"] != id]
+    salvar_produtos(produtos)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
